509ce6f678e859880aeb50ab78c834dfa8f3eac6
Kamil <k.prosciewicz@gmail.com>
2014-09-10 20:18:39 +0000

    4/99

---
+++ src/Functions.hs
@@ -0,0 +1,35 @@
+module Functions where
+    fib :: Int -> Int
+    fib x
+        | x > 1 = fib(x-1) + fib(x-2)
+        | x == 0 = 0
+        | x == 1 = 1
+        | otherwise = error "Nie można"
+
+    silnia :: Int -> Int
+    silnia n
+        | n == 0 = 1
+        | n >= 1 = n * silnia(n-1)
+        | otherwise = error "Liczba ujemna"
+
+    binomial n k
+        | k == 0 || k == n = 1
+        | 0 < k && k < n = binomial (n-1) (k-1) + binomial (n-1) k
+        | otherwise = error "wut"
+    last' :: (Ord a) => [a] -> a
+    last' [] = error ""
+    last' [a] = a
+    last' (x:xs) = last' xs
+
+    lastButOne' :: (Ord a) => [a] -> a
+    lastButOne' [] = error ""
+    lastButOne' [a, _] = a
+    lastButOne' (x:xs) = lastButOne' xs
+
+    elementAt' :: (Ord a, Integral b) => [a] -> b -> a
+    elementAt' (x:_) 1 = x
+    elementAt' (_:xs) k = elementAt' xs (k-1)
+
+    length' :: (Ord a, Integral b) => [a] -> b
+    length' [] = 0
+    length' (_:xs) = 1+ length' xs
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,18 @@
 -- | Main entry point to the application.
 module Main where
 
+import Functions
+
 -- | The main entry point.
 main :: IO ()
 main = do
     putStrLn "Welcome to FP Haskell Center!"
     putStrLn "Have a good day!"
+    print (fib 5)
+    print (silnia 10)
+    print (binomial 2 2)
+    print (last' [1, 2 ..15] )
+    print (lastButOne' [1, 2 ..15] )
+    print (elementAt' [1..100] 100)
+    print (length' "Kamil")
+    --reverse
